e20d5ba7ff71be0ca1fb5c1feb83f6e692c7f499
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-09-14 13:48:05 -0400

    testworks💪(i hope)

--- fdraft.cpp
+++ fdraft.cpp
@@ -41,9 +41,9 @@ int main() {
 
 bool check(int order[], string blocks[]){
      int g=0;
-     string leters[8]={a,b,c,d,e,f,g,h};
+     string leters[8]={"a","b","c","d","e","f","g","h"};
      while (g<8){
-        if (blocks[order[g]].find(leters[g])== -1){
+        if (blocks[order[g]].find(leters[g]) == -1){
             return false;
         }
         g++;
