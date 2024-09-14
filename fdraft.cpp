dded727bdadc564b1ba13ace8f25ad688ee168da
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-09-14 02:12:02 -0400

    blablabla

--- fdraft.cpp
+++ fdraft.cpp
@@ -1,25 +1,25 @@
 #include <iostream>
 #include <string>
 using namespace std;
-// a a,b,c b b,c,d d a,b,c e d,e,f f a,g,h g c,e,g h a,e,g
+// a a,b,c b b,c,d c f,g,h d a,b,c e d,e,f f a,g,h g c,e,g h a,e,g
 int main() {
-    int i=1;
-    string classes[9];
-    string blocks[9];
-    string nocma[9];
-    int order[9];
+    int i=0;
+    string classes[8];
+    string blocks[8];
+    string nocma[8];
+    int order[8];
 
-    while (i<9){
+    while (i<8){
         cout << "enter the name or your class :";
         cin >> classes[i];
         i++;
         cout << "enter the blocks the class is offered seperated by commas:";
         cin >> blocks[i];
     }
-    i=1;
+    i=0;
     int c=0;
     int len=0;
-    while(i<9){
+    while(i<8){
         //tolower(blocks[i]);
         len=blocks[i].length();
         while(c<len){
@@ -31,8 +31,8 @@ int main() {
         c=0;
         i++;
     }
-    i=1;
-    // while (i<9){
+    i=0;
+    // while (i<8){
     //     cout << classes[i] << " " << blocks[i] << endl;
     //     i++;
     // }
