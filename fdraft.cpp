c0a17b469774f740c57b51f4030598ed584af3ae
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-09-14 00:49:40 -0400

    gonnadiesaveme

--- fdraft.cpp
+++ fdraft.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     int i=1;
     string classes[9];
-    char blocks[9];
-    string numbl[9];
+    string blocks[9];
+    int nocma[9];
+    int order[9];
     while (i<9){
         cout << "enter the name or your class :";
         cin >> classes[i];
@@ -14,15 +16,29 @@ int main() {
         cin >> blocks[i];
     }
     i=1;
+    int c=0;
+    int len=0;
     while(i<9){
         tolower(blocks[i]);
+        len=length(blocks[i])
+        while(c<len){
+            numbl[i]=numbl[i]+blocks[i].substr(i,1)
+            cout << blocks[i].substr(i,1) << endl;
+            cout << nocma[i];
+            c++;
+            c++;
+        }
         i++;
     }
     i=1;
     while (i<9){
         cout << classes[i] << " " << blocks[i] << endl;
-        cout << classes[0];
         i++;
     }
     return 0;
+}
+
+bool check(order[],blocks[]){
+    return true;
+
 }
