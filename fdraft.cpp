71191e972219e46f5493ae4cbcdbc496b1a2fc6f
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-09-14 01:06:10 -0400

    mewhencode

--- fdraft.cpp
+++ fdraft.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
+// a a,b,c b b,c,d d a,b,c e d,e,f f a,g,h g c,e,g h a,e,g
 int main() {
     int i=1;
     string classes[9];
     string blocks[9];
-    int nocma[9];
+    string nocma[9];
     int order[9];
+
     while (i<9){
         cout << "enter the name or your class :";
         cin >> classes[i];
@@ -19,26 +20,26 @@ int main() {
     int c=0;
     int len=0;
     while(i<9){
-        tolower(blocks[i]);
-        len=length(blocks[i])
+        //tolower(blocks[i]);
+        len=blocks[i].length();
         while(c<len){
-            numbl[i]=numbl[i]+blocks[i].substr(i,1)
-            cout << blocks[i].substr(i,1) << endl;
-            cout << nocma[i];
+            nocma[i]=nocma[i]+blocks[i].substr(c,1);
+            cout << nocma[i] << endl;
             c++;
             c++;
         }
+        c=0;
         i++;
     }
     i=1;
-    while (i<9){
-        cout << classes[i] << " " << blocks[i] << endl;
-        i++;
-    }
+    // while (i<9){
+    //     cout << classes[i] << " " << blocks[i] << endl;
+    //     i++;
+    // }
     return 0;
 }
 
-bool check(order[],blocks[]){
-    return true;
+// bool check(order[],blocks[]){
+//     return true;
 
-}
+// }
