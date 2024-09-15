dafeff2f19fdef7f92f169ea920b62c4e4e68862
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-09-14 22:58:20 -0400

    E it doesnt work plz help me

--- fdraft.cpp
+++ fdraft.cpp
@@ -1,44 +1,7 @@
 #include <iostream>
 #include <string>
 using namespace std;
-// a a,b,c b b,c,d c f,g,h d a,b,c e d,e,f f a,g,h g c,e,g h a,e,g
-int main() {
-    int i=0;
-    string classes[8];
-    string blocks[8];
-    string nocma[8];
-    int order[8];
-
-    while (i<8){
-        cout << "enter the name or your class :";
-        cin >> classes[i];
-        i++;
-        cout << "enter the blocks the class is offered seperated by commas:";
-        cin >> blocks[i];
-    }
-    i=0;
-    int c=0;
-    int len=0;
-    while(i<8){
-        //tolower(blocks[i]);
-        len=blocks[i].length();
-        while(c<len){
-            nocma[i]=nocma[i]+blocks[i].substr(c,1);
-            cout << nocma[i] << endl;
-            c++;
-            c++;
-        }
-        c=0;
-        i++;
-    }
-    i=0;
-    // while (i<8){
-    //     cout << classes[i] << " " << blocks[i] << endl;
-    //     i++;
-    // }
-    return 0;
-}
-
+//testing: a a,b,c b b,c,d c f,g,h d a,b,c e d,e,f f a,g,h g c,e,g h a,e,g
 bool check(int order[], string blocks[]){
      int g=0;
      string leters[8]={"a","b","c","d","e","f","g","h"};
@@ -49,5 +12,66 @@ bool check(int order[], string blocks[]){
         g++;
      }
      return true;
-
  }
+
+int main() {
+    int i=0;
+    string classes[8];
+    string blocks[8];
+    string nocma[8];
+    int order[8]={0,1,2,3,4,5,6,7};
+    int combos[8][8];
+
+    while (i<8){
+        cout << "enter the name or your class :";
+        cin >> classes[i];
+        i++;
+        cout << "enter the blocks the class is offered seperated by commas:";
+        cin >> blocks[i];
+    }
+    i=0;
+    int c=0;
+
+    //makesallcombinations
+    i=0;
+    c=0;
+    int com=0;
+    int b=0;
+    while(i<8){
+        while(c<8){
+            order[i]=c;
+            order[c]=i;
+            if (check(order,classes)){
+                while (b<8){
+                    combos[b][com] = order[b];
+                    b++;
+                }
+                com++;
+            }
+            c++;
+        }
+        i++;
+    }
+
+    // while (i<8){
+    //     cout << classes[i] << " " << blocks[i] << endl;
+    //     i++;
+    // }
+        // int len=0;
+    //removes the commas not needed but its here
+    // while(i<8){
+    //     //tolower(blocks[i]);
+    //     len=blocks[i].length();
+    //     while(c<len){
+    //         nocma[i]=nocma[i]+blocks[i].substr(c,1);
+    //         c++;
+    //         c++;
+    //     }
+    //     c=0;
+    //     i++;
+    // }
+
+    return 0;
+}
+     
+     
--- testing.md
+++ testing.md
@@ -16,9 +16,36 @@ bool check(int order[], string blocks[]){
 
  }
  int main(){
-    string b[8] = {"g,a","d,a","a","h,d","b,c","c","e","f"};
+    string z[8] = {"g,a","d,a","a","h,d","b,c","c","e","f"};
     int o[8] = {2,4,5,1,6,7,0,3};
-    bool plzwork = check(o,b);
-    cout << plzwork;
+    int combos [10][8];
     
+    int i=0;
+    int c=0;
+    int com=0;
+    int bo=0;
+    while(i<8){
+        while(c<8){
+            o[i]=c;
+            o[c]=i;
+            if (check(o,z)){
+                while (bo<8){
+                    combos[bo][com] = o[bo];
+                    bo++;
+                }
+                com++;
+            }
+            c++;
+        }
+        i++;
+    }
+    int g=0;
+    int f=0;
+    while (g<sizeof(combos[0]) / sizeof (combos[0][0])){
+        while(f<8){
+            cout <<combos[g][f];
+            f++;
+        }
+        g++;
+    }
 }
