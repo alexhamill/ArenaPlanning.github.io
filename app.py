a695bbf9d20c2bab3907c9b93084cc1561e3cd22
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-09-19 13:10:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -13,3 +15,9 @@ def process():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
